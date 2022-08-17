a08b7795d0b1aba38bedf78a0c627e052f1cde50
Jean-Bernard Le Blanc <108352489+CalmChip@users.noreply.github.com>
2022-08-17 15:46:08 -0400

    Test Path

--- testPath.js
+++ testPath.js
@@ -8,6 +8,18 @@ console.log("Affichage de __dirname", __dirname);
 
 console.log(`Affichage de path.delimiter: ${path.delimiter}`);
 console.log(`Affichage de path.extname: ${path.extname(__filename)}`);
-console.log("Affichage de path.pase", path.parse(__filename));
+console.log("Affichage de path.parse", path.parse(__filename));
 const pathParse = path.parse(__filename);
 console.log(pathParse.name); //Aller chercher une seul partie
+console.log(pathParse.root); //Aller chercher une seul partie
+console.log(pathParse.dir); //Aller chercher une seul partie
+console.log("Affichage de path.parse.ext", path.parse(__filename).ext);
+console.log(
+  "construction d'un chemin de fichier ",
+  path.join(__dirname, "mesFichiers", "toto.txt")
+);
+
+const fs = require("fs");
+
+// fs.mkdirSync(path.join(__dirname, "MesFichiers", "test99"));  //Creer un fichier a cet endroit
+fs.unlinkSync(path.join(__dirname, "MesFichiers", "toto.txt"));
